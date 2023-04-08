9f37a0d3f8de2e0268357c3ddb9bdbdecf582030
Peter Geertsema <peterjgeertsema@gmail.com>
2023-04-08 13:50:14 -0400

    write code

---
+++ lambda.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ["API_KEY"]
+
+def get_distance(university, destination):
+    """
+    Returns the time in seconds for to get from one location to another using the google maps API.
+    """
+
+    # Get the distance from the google maps API
+    url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={0}&destinations={1}&key={2}'.format(university, destination, API_KEY)
+    response = requests.get(url)
+    data = response.json()
+    distance = data['rows'][0]['elements'][0]['duration']['value']
+
+    return distance
+
+get_distance("University of California, Berkeley", "San Francisco, CA")
