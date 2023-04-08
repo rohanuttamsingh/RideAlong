13f65a3b6ba0935cafb34274b64b86415fdf13f3
Peter Geertsema <peterjgeertsema@gmail.com>
2023-04-08 18:58:48 -0400

    get matching users

--- confirm_ride.py
+++ confirm_ride.py
@@ -2,27 +2,24 @@
 Utilities for confirming a rider and a driver.
 """
 
-def handle_rider_response(phone_number: str, body: str):
+from utils import read_table, send_text
+
+
+def confirm(phone_number: str):
     """
     Receives and processes a response from a rider.
-    
+
     Args:
         phone_number: The phone number of the rider.
-        body: The body of the rider's response.
     """
-    if body.capitalize() != 'Yes':
-        return
+    # Remove the leading +1.
+    phone_number = phone_number[2:]
 
-    # If confirmed, send a text to the driver.
-    
+    # Read our database.
+    user_table = read_table('Users')
+    # Convert phone numbers to strings.
+    user_table["Phone"] = user_table["Phone"].astype(str)
+    notification_table = read_table('Notifications')
 
-def handle_driver_response(phone_number: str, body: str):
-    """
-    Receives and processes a response from a driver.
-    
-    Args:
-        phone_number: The phone number of the driver.
-        body: The body of the driver's response.
-    """
-    # If confirmed, send a text to both the rider and the driver.
-
+    # Find the corresponding user.
+    users = user_table[user_table['Phone'] == phone_number]
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 from twilio.rest import Client
+import pandas as pd
 
 load_dotenv()
 ACCOUNT_TOKEN = os.environ["ACCOUNT_TOKEN"]
 AUTH_TOKEN = os.environ["AUTH_TOKEN"]
+
 
 def send_text(number: str, message: str):
     """
@@ -19,3 +21,14 @@ def send_text(number: str, message: str):
         to=number
     )
     print(message.sid)
+
+
+def read_table(sheet_name: str) -> pd.DataFrame:
+    """
+    """
+    SHEET_ID = '1UuV__YRrPOeS2ccDkp0A7r5Knsyvnf_bHF2YymmVBq4'
+    url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+    df = pd.read_csv(url)
+    # Remove columns that are all NaN.
+    df = df.dropna(axis=1, how='all')
+    return df
